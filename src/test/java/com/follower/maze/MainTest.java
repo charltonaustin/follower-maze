92c13af1338b12c7511de95257cccf87523dbda1
charlie austin <charlie.austin@intentmedia.com>
2015-09-25 06:55:41 -0400

    Adding in a first distribution of the application.

---
+++ src/test/java/com/follower/maze/MainTest.java
@@ -0,0 +1,14 @@
+package com.follower.maze;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void test() throws Exception {
+        assertTrue(true);
+    }
+
+}
